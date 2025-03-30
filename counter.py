5509c23b4ee49450a6981ec7f0bec4e8d5bd061a
Diode-exe <DiodesCAER@protonmail.com>
2025-03-29 19:22:02 -0500

    Added infinite counter

---
+++ counter.py
@@ -0,0 +1,6 @@
+print("Welcome to the infinite counter! This will make your CPU very warm, depending on your cooling solution.")
+number = 0
+delay = input("Enter the amount of time to wait before calculating the next number: ")
+while True:
+    number = number + 1
+    print(number)
